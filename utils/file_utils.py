69af281065436a379be34c2db2cdf75e6c01acbd
liuyi <412805318@qq.com>
2021-10-20 11:50:42 +0800

    文件相关操作的辅助方法

---
+++ utils/file_utils.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+def store_HID(hids: list):  # TODO
+    """存储批量HID到本地"""
+    pass
+
+
+def read_license_by_HID(hid: str) -> list:  # TODO
+    """根据HID, 从HID-License map表中获取对应的License,
+    返回[HID-组件ID-License, ...]的列表"""
+    pass
+
+
+def record_HID_activated(hid: str) -> None:  # TODO
+    """存储HID到指定本地文件"""
+    pass
